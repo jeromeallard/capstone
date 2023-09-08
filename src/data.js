be7f38c66f238f12a6f9a217f4c69e4eb5dcc132
jeromeallard <jeromematthew.m@gmail.com>
2023-09-08 20:09:58 +0300

    adding menu data

---
+++ src/data.js
@@ -0,0 +1,89 @@
+import appetizer1 from "./images/menu/garlicbread.jpg";
+import appetizer2 from "./images/menu/crabstuffedmushrooms.jpg";
+import appetizer3 from "./images/menu/spinachartichokedip.jpg";
+import main1 from "./images/menu/shrimpalfredo.jpg";
+import main2 from "./images/menu/sesamechicken.jpg";
+import main3 from "./images/menu/pastramisandwich.jpg";
+import main4 from "./images/menu/porkbellyadobo.jpg";
+import main5 from "./images/menu/grilledribeye.jpg";
+import main6 from "./images/menu/biscuitsandgravy.jpg";
+import dessert1 from "./images/menu/mangopannacota.jpg";
+import dessert2 from "./images/menu/strawberryhoneytoast.jpg";
+import dessert3 from "./images/menu/cheesecakecups.jpg";
+
+const menu = [
+  {
+    type: "appetizer",
+    name: "Garlic Bread",
+    image: appetizer1,
+    price: "$5.99",
+  },
+  {
+    type: "appetizer",
+    name: "Crab Stuffed Mushrooms",
+    image: appetizer2,
+    price: "$7.99",
+  },
+  {
+    type: "appetizer",
+    name: "Spinach Artichoke Dip",
+    image: appetizer3,
+    price: "$6.99",
+  },
+  {
+    type: "main",
+    name: "Shrimp Alfredo",
+    image: main1,
+    price: "$13.99",
+  },
+  {
+    type: "main",
+    name: "Sesame Chicken",
+    image: main2,
+    price: "$11.99",
+  },
+  {
+    type: "main",
+    name: "Pastrami Sandwich and Fries",
+    image: main3,
+    price: "$12.99",
+  },
+  {
+    type: "main",
+    name: "Pork Belly Adobo",
+    image: main4,
+    price: "$11.99",
+  },
+  {
+    type: "main",
+    name: "Grilled Ribeye",
+    image: main5,
+    price: "$12.99",
+  },
+  {
+    type: "main",
+    name: "Biscuits and gravy",
+    image: main6,
+    price: "$9.99",
+  },
+  {
+    type: "dessert",
+    name: "Mango Panna Cota",
+    image: dessert1,
+    price: "$5.99",
+  },
+  {
+    type: "dessert",
+    name: "Strawberry Honey Toast",
+    image: dessert2,
+    price: "$7.99",
+  },
+  {
+    type: "dessert",
+    name: "Cheesecake Cups",
+    image: dessert3,
+    price: "$5.99",
+  },
+];
+
+export default menu;
