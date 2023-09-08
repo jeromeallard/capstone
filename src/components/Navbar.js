275477b22b0e63e2f13a13af2cf2e22e8c599343
jeromeallard <jeromematthew.m@gmail.com>
2023-09-08 06:37:50 +0300

    modified:   src/components/Navbar.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,43 @@
+import { NavLink } from "react-router-dom";
+
+const style = {
+  navActive: "nav-font px-3 nav-item nav-link nav-active",
+  navNonActive: "nav-font px-3 nav-item nav-link hover",
+};
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-light bg-light pd mb-3">
+      <div className="container">
+        <NavLink className="navbar-brand" to="/">
+          <img className="logo-height img-fluid" src={LLLogo} alt="Little Lemon Logo" />
+        </NavLink>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+          <div className="navbar-nav mx-auto">
+            <NavLink className={({ isActive }) => (isActive ? style.navActive : style.navNonActive)} to="/">
+              HOME
+            </NavLink>
+            <NavLink className={({ isActive }) => (isActive ? style.navActive : style.navNonActive)} to="/reserve">
+              RESERVE
+            </NavLink>
+            <NavLink className={({ isActive }) => (isActive ? style.navActive : style.navNonActive)} to="/menu">
+              MENU
+            </NavLink>
+            <NavLink className={({ isActive }) => (isActive ? style.navActive : style.navNonActive)} to="/about">
+              ABOUT US
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
